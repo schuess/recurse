6636a23d3b42f29b82f9d2e2a70e29820359ff24
Robert Schuessler <github@schu.us>
2017-12-09 16:32:03 -0500

    AoC Day 9

--- aoc/day9.py
+++ aoc/day9.py
@@ -6,11 +6,11 @@ with open('./data/day9.in') as f: text_raw = f.read()
 
 text_minus_cancels = re.sub(r'!.', '', text_raw)
 text_minus_garbage, num_matches = re.subn(r'<.*?>', '', text_minus_cancels)
-garbage_count = len(text_minus_cancels) - (len(text_minus_garbage) + 2 * num_matches)
+garbage_count = len(text_minus_cancels) - len(text_minus_garbage) - 2 * num_matches
 
 score = level = 0
 
-for c in list(text_minus_garbage):
+for c in text_minus_garbage:
     if c == '{':
         level += 1
     if c == '}':
